55752b9713e474f95f199b6f8e4258db9bf21e3c
Fatih Emre Özçelik <fatihemreozcelik@192.168.1.126>
2026-06-18 22:53:42 +0300

    AeroPico FC temel kod iskeleti oluşturuldu ve derleme hataları giderildi

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,30 @@
+
+#include <Arduino.h>
+
+// Kendi dosyalarımızı dahil ediyoruz
 #include "config.h"
+#include "Sensors.h"
+#include "RX.h"
+#include "IMU.h"
+#include "Output.h"
+
+void setup() {
+  // Seri portu başlat (GCS için hazırlık)
+  Serial.begin(115200);
+  
+  // Sensörleri ve diğer modülleri başlat
+  initSensors();
+  
+  Serial.println("AeroPico FC Baslatildi!");
+}
+
+void loop() {
+  // Sensörlerden verileri oku
+  updateSensors();
+  
+  // Uçuş hesaplamalarını yap
+  // updateIMU();
+  
+  // Motorlara komut gönder
+  // updateOutput();
+}
