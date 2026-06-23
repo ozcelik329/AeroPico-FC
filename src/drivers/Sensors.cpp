9225958d354f144bebd4a15e4f0484e9ed9e85dd
Fatih Emre Özçelik <fatihemreozcelik@192.168.1.198>
2026-06-23 15:50:53 +0300

    WIP: save changes before pushing to hardware branch

--- src/drivers/Sensors.cpp
+++ src/drivers/Sensors.cpp
@@ -1,60 +1,80 @@
 #include "Sensors.h"
 
 void SensorManager::init() {
+    mutex_init(&_mutex);
+
+    _buf[0] = {};
+    _buf[1] = {};
+
     Wire.begin();
     Wire.setClock(400000);
 
     if (!mpu.begin()) {
-        Serial.println("MPU6050 bulunamadi!");
+        Serial.println("[SENSOR] MPU6050 bulunamadi!");
     } else {
         mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
         mpu.setGyroRange(MPU6050_RANGE_500_DEG);
         mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+        Serial.println("[SENSOR] MPU6050 hazir.");
     }
 
     #ifdef USE_GY87
-      hasMag = mag.begin();
-      if (!hasMag) {
-          Serial.println("HMC5883L (magnetometre) bulunamadi!");
-      }
+        hasMag = mag.begin();
+        if (!hasMag) Serial.println("[SENSOR] HMC5883L bulunamadi!");
+        else         Serial.println("[SENSOR] HMC5883L hazir.");
 
-      hasBaro = bmp.begin();
-      if (!hasBaro) {
-          Serial.println("BMP085 (barometre) bulunamadi!");
-      }
+        hasBaro = bmp.begin();
+        if (!hasBaro) Serial.println("[SENSOR] BMP085 bulunamadi!");
+        else          Serial.println("[SENSOR] BMP085 hazir.");
     #endif
 }
 
 void SensorManager::update() {
+    // Yazılacak buffer'ı belirle (aktif olmayan)
+    uint8_t writeIdx = 1 - _writeIdx;
+    SensorBuffer& buf = _buf[writeIdx];
+
     sensors_event_t a, g, temp;
     mpu.getEvent(&a, &g, &temp);
 
-    this->ax = a.acceleration.x;
-    this->ay = a.acceleration.y;
-    this->az = a.acceleration.z;
-    this->gx = g.gyro.x;
-    this->gy = g.gyro.y;
-    this->gz = g.gyro.z;
+    buf.ax = a.acceleration.x;
+    buf.ay = a.acceleration.y;
+    buf.az = a.acceleration.z;
+    buf.gx = g.gyro.x;
+    buf.gy = g.gyro.y;
+    buf.gz = g.gyro.z;
+    buf.timestamp = micros();
+    buf.valid = true;
 
     #ifdef USE_GY87
-      if (hasMag) {
-          sensors_event_t mag_event;
-          mag.getEvent(&mag_event);
-          this->mx = mag_event.magnetic.x;
-          this->my = mag_event.magnetic.y;
-          this->mz = mag_event.magnetic.z;
-      } else {
-          this->mx = 0.0f;
-          this->my = 0.0f;
-          this->mz = 0.0f;
-      }
+        if (hasMag) {
+            sensors_event_t mag_event;
+            mag.getEvent(&mag_event);
+            buf.mx = mag_event.magnetic.x;
+            buf.my = mag_event.magnetic.y;
+            buf.mz = mag_event.magnetic.z;
+        } else {
+            buf.mx = buf.my = buf.mz = 0.0f;
+        }
 
-      if (hasBaro) {
-          sensors_event_t bmp_event;
-          bmp.getEvent(&bmp_event);
-          this->pressure = bmp_event.pressure;
-      } else {
-          this->pressure = 1013.25f;
-      }
+        if (hasBaro) {
+            sensors_event_t bmp_event;
+            bmp.getEvent(&bmp_event);
+            buf.pressure = bmp_event.pressure;
+        } else {
+            buf.pressure = 1013.25f;
+        }
     #endif
+
+    // Atomik index değişimi — okuyucu her zaman tam veriyi görür
+    mutex_enter_blocking(&_mutex);
+    _writeIdx = writeIdx;
+    mutex_exit(&_mutex);
+}
+
+SensorBuffer SensorManager::getLatest() {
+    mutex_enter_blocking(&_mutex);
+    SensorBuffer copy = _buf[_writeIdx];
+    mutex_exit(&_mutex);
+    return copy;
 }
--- src/drivers/Sensors.h
+++ src/drivers/Sensors.h
@@ -11,28 +11,42 @@
   #include <Adafruit_BMP085_U.h>
 #endif
 
+// DMA-ready ping-pong buffer
+struct SensorBuffer {
+    float ax, ay, az;
+    float gx, gy, gz;
+    #ifdef USE_GY87
+        float mx, my, mz;
+        float pressure;
+    #endif
+    uint32_t timestamp;
+    bool valid;
+};
+
 class SensorManager {
   public:
     void init();
     void update();
 
-    // Ham veriler
-    float ax, ay, az;
-    float gx, gy, gz;
-    
+    // PID tarafı bu fonksiyonu çağırır — mutex korumalı
+    SensorBuffer getLatest();
+
     #ifdef USE_GY87
-      float mx, my, mz;
-      float pressure;
-      bool hasMag;
-      bool hasBaro;
+        bool hasMag  = false;
+        bool hasBaro = false;
     #endif
 
   private:
     Adafruit_MPU6050 mpu;
     #ifdef USE_GY87
-      Adafruit_HMC5883_Unified mag;
-      Adafruit_BMP085_Unified bmp;
+        Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+        Adafruit_BMP085_Unified  bmp = Adafruit_BMP085_Unified(10085);
     #endif
+
+    // Ping-pong buffer — biri yazılırken diğeri okunur
+    SensorBuffer _buf[2];
+    volatile uint8_t _writeIdx = 0;
+    mutex_t _mutex;
 };
 
 #endif
