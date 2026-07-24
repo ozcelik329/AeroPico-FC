5f612172ed4e1099bbb2741c36c07d7528d1b3e0
Fatih Emre Ozcelik <ozcelik329@users.noreply.github.com>
2026-07-25 02:52:34 +0300

    Add homepage overview hub

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Configurator from "../components/Configurator.jsx";
 import Hero from "../components/Hero.jsx";
+import HomeOverview from "../components/HomeOverview.jsx";
 import QuickStart from "../components/QuickStart.jsx";
 import PageShell from "../components/layout/PageShell.jsx";
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <PageShell>
       <Hero />
+      <HomeOverview />
       <QuickStart />
       <Configurator />
     </PageShell>
