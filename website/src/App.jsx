8d4421d0b9033547664ae4fce94df0dc8ffd9f9e
Fatih Emre Ozcelik <ozcelik329@users.noreply.github.com>
2026-07-25 02:41:00 +0300

    Move hardware list to separate page

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,14 +1,17 @@
 import { ModalProvider } from "./context/ModalContext.jsx";
 import { LanguageProvider } from "./context/LanguageContext.jsx";
+import HardwarePage from "./pages/HardwarePage.jsx";
 import Home from "./pages/Home.jsx";
 import ReleasesModal from "./components/ui/ReleasesModal.jsx";
 import SpecsModal from "./components/ui/SpecsModal.jsx";
 
 export default function App() {
+  const route = window.location.pathname === "/hardware" ? "hardware" : "home";
+
   return (
     <LanguageProvider>
       <ModalProvider>
-        <Home />
+        {route === "hardware" ? <HardwarePage /> : <Home />}
         <ReleasesModal />
         <SpecsModal />
       </ModalProvider>
